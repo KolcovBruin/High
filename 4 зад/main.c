1e863ea35e3472fdbfa2582c1d2541dff668fba7
GR <grisha25062@icloud.com>
2019-10-14 17:20:03 +0300

    Initial Commit

---
+++ 4 зад/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  4 зад
+//
+//  Created by Григорий on 14.10.2019.
+//  Copyright © 2019 Григорий. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
