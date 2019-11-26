2a51ce1672136e0440d47a1c0b147da9a6e71a82
KolcovBruin <47564050+KolcovBruin@users.noreply.github.com>
2019-11-26 11:17:01 +0300

    Для указ

--- 2 лаба ЯВУ/main.c
+++ 2 лаба ЯВУ/main.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
-
+#define MAXLINE 100
 int main(int argc,char * argv[])
 {
    
     FILE *fpin=fopen(argv[1], "rt" );
      FILE *fpout=fopen(argv[2], "wt" );
-    char lit;
-    int i=0;
-  while((lit=fgetc(fpin))!=EOF)
+    char line[MAXLINE];
+    char *ptr;
+    ptr = fgets( line, MAXLINE, fpin );
+  while((!feof(fpin))&&(ptr!=NULL))
+      
   {
-      (( lit>='a')&&(lit<='z'))?(lit-='!'-1), fprintf(fpout,"%c",lit): fprintf(fpout,"%c",lit);
+      while (*ptr!='\0')
+      {
+      (( *ptr>='a')&&(*ptr<='z'))?((*ptr-='!'-1), fprintf(fpout,"%c",*ptr)): (fprintf(fpout,"%c",*ptr));
+          ptr++;
+      }
+      ptr = fgets( line, MAXLINE, fpin );
   }
   return 0;
 }
